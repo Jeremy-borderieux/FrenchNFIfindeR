52cacf169e1442f589e398d45fad6674dc5502fe
Jeremy BORDERIEUX <borderieux@agroparistech.fr>
2023-01-27 16:08:32 +0100

    Added compute_dendro_sp some english names

--- R/F_main_func.R
+++ R/F_main_func.R
@@ -32,9 +32,9 @@ download_NFI<-function(dir=getwd(),
 #' The NFI is seperated into different csv files or R data.table, that you can merge by idp
 #' \itemize{
 #' \item{**NFI_plot_info**}{ The metadata such as position, or time of measurment of a NFI plot, plot are identified with a unique ID called idp}
-#' \item{**NFI_ecologie**}{ Informations regarding soil, understory cover observations}
-#' \item{**NFI_arbres**}{ Living tree measurements and informations}
-#' \item{**NFI_dendro**}{ Dendrologic variable computed with NFI_arbres}
+#' \item{**NFI_ecology**}{ Informations regarding soil, understory cover observations}
+#' \item{**NFI_tree**}{ Living tree measurements and informations}
+#' \item{**NFI_dendro**}{ Dendrologic variable computed with NFI_tree}
 #' \item{**NFI_cover**}{ Relative and absolute cover of every tree species recorded}
 #' \item{**NFI_flora**}{ Florisitic surveys of the NFI plot}
 #' }
@@ -72,17 +72,17 @@ get_NFI<-function(dir=getwd(),
 
 
   ## reading them
-  NFI_ecologie<-data.table(read.table(unz(path_zip,"ECOLOGIE.csv"),sep=";",header=T,stringsAsFactors =F))
+  NFI_ecology<-data.table(read.table(unz(path_zip,"ECOLOGIE.csv"),sep=";",header=T,stringsAsFactors =F))
   NFI_plot_info<-data.table(read.table(unz(path_zip,"PLACETTE.csv"),sep=";",header=T,stringsAsFactors =F))
-  NFI_arbres<-data.table(read.table(unz(path_zip,"ARBRE.csv"),sep=";",header=T,stringsAsFactors =F))
+  NFI_tree<-data.table(read.table(unz(path_zip,"ARBRE.csv"),sep=";",header=T,stringsAsFactors =F))
   NFI_cover<-data.table(read.table(unz(path_zip,"COUVERT.csv"),sep=";",header=T,stringsAsFactors =F))
   NFI_flora<-data.table(read.table(unz(path_zip,"FLORE.csv"),sep=";",header=T,stringsAsFactors =F))
 
   if(!any(c(visit==1 , visit==2 , visit==c(1,2))))stop("Need a valid visit code")
 
-  colnames(NFI_ecologie)<-tolower(colnames(NFI_ecologie));colnames(NFI_ecologie)[1]<-"campagne";NFI_ecologie[,x:=NULL]
+  colnames(NFI_ecology)<-tolower(colnames(NFI_ecology));colnames(NFI_ecology)[1]<-"campagne";NFI_ecology[,x:=NULL]
   colnames(NFI_plot_info)<-tolower(colnames(NFI_plot_info));colnames(NFI_plot_info)[1]<-"campagne";NFI_plot_info[,x:=NULL]
-  colnames(NFI_arbres)<-tolower(colnames(NFI_arbres));colnames(NFI_arbres)[1]<-"campagne";NFI_arbres[,x:=NULL]
+  colnames(NFI_tree)<-tolower(colnames(NFI_tree));colnames(NFI_tree)[1]<-"campagne";NFI_tree[,x:=NULL]
   colnames(NFI_cover)<-tolower(colnames(NFI_cover));colnames(NFI_cover)[1]<-"campagne";NFI_cover[,x:=NULL]
   colnames(NFI_flora)<-tolower(colnames(NFI_flora));colnames(NFI_flora)[1]<-"campagne";NFI_flora[,x:=NULL]
 
@@ -99,33 +99,33 @@ get_NFI<-function(dir=getwd(),
   #   NFI_plot_info[,gest_struct:=as.character(gest_struct)]
   #   NFI_plot_info[,tmp:=NULL]
 
-  NFI_ecologie[,campagne:=NULL]
+  NFI_ecology[,campagne:=NULL]
 
   # Imputation aux arbres simplifi?s de la valeur moyenne des mesures des arbres de la m?me placette, essences, et classe de taille
-  NFI_arbres[, dimess := cut(c13, breaks = c(0, 70.5, 117.5, 164.5, 1000)/100, labels = c("PB", "BM", "GB", "TGB"), right = FALSE)]
-  NFI_arbres[, htot := ifelse(is.na(htot), mean(htot, na.rm = TRUE), htot), by = c("idp", "espar", "dimess")]
-  NFI_arbres[, ir5 := ifelse(is.na(ir5), mean(ir5, na.rm = TRUE), ir5), by = c("idp", "espar", "dimess")]
+  NFI_tree[, dimess := cut(c13, breaks = c(0, 70.5, 117.5, 164.5, 1000)/100, labels = c("PB", "BM", "GB", "TGB"), right = FALSE)]
+  NFI_tree[, htot := ifelse(is.na(htot), mean(htot, na.rm = TRUE), htot), by = c("idp", "espar", "dimess")]
+  NFI_tree[, ir5 := ifelse(is.na(ir5), mean(ir5, na.rm = TRUE), ir5), by = c("idp", "espar", "dimess")]
 
-  NFI_arbres[,campagne_init := floor(idp/100000)+2005]
+  NFI_tree[,campagne_init := floor(idp/100000)+2005]
   # Definition d'une classe de sous-?tage
-  NFI_arbres[,strate:=ifelse(htot>0.5*max(htot, na.rm = T), "etagePrincipal", "sousEtage"),keyby=idp]
+  NFI_tree[,strate:=ifelse(htot>0.5*max(htot, na.rm = T), "etagePrincipal", "sousEtage"),keyby=idp]
 
 
   ### Calcul des surfaces terri?res
-  NFI_arbres[,gFinal:=c13*c13*w/(4*pi)]
-  NFI_arbres[,gInitial:=pi*(c13/(2*pi)-ir5/10)^2*w]
+  NFI_tree[,gFinal:=c13*c13*w/(4*pi)]
+  NFI_tree[,gInitial:=pi*(c13/(2*pi)-ir5/10)^2*w]
 
   ## selection arbres vivants pour la dendro
-  NFI_arbres_vivants<-NFI_arbres[veget%in%c("0","Z"),]
+  NFI_tree_vivants<-NFI_tree[veget%in%c("0","Z"),]
 
   ## inclure l'état de l'arbre en revisite
-  NFI_arbres_revisite<-NFI_arbres[veget=="",]
-  NFI_arbres<-merge(NFI_arbres,NFI_arbres_revisite[,c("idp","a","veget5")],by=c("idp","a"),all.x = T,suffixes = c("","_tmp"))
-  NFI_arbres[,veget5:=veget5_tmp]
-  NFI_arbres[,veget5_tmp:=NULL]
+  NFI_tree_revisite<-NFI_tree[veget=="",]
+  NFI_tree<-merge(NFI_tree,NFI_tree_revisite[,c("idp","a","veget5")],by=c("idp","a"),all.x = T,suffixes = c("","_tmp"))
+  NFI_tree[,veget5:=veget5_tmp]
+  NFI_tree[,veget5_tmp:=NULL]
 
   ### Valeurs dendro ?chelle placette
-  NFI_dendro <- NFI_arbres_vivants[,.(basal_area=sum(gFinal),
+  NFI_dendro <- NFI_tree_vivants[,.(basal_area=sum(gFinal),
                               Ntot=sum(w)),  by = idp]
 
 
@@ -153,19 +153,19 @@ get_NFI<-function(dir=getwd(),
   colnames(tree_NFI_codetaxo)<-c("espar","french_name","cd_ref","species_name")
   tree_NFI_codetaxo[,espar:=ifelse(nchar(espar)==1,paste0("0",espar),espar)]
 
-  NFI_arbres<-merge(NFI_arbres,tree_NFI_codetaxo[,c("espar","species_name","french_name")],by="espar",all.x = T)
-  NFI_arbres<-NFI_arbres[veget!="",]
+  NFI_tree<-merge(NFI_tree,tree_NFI_codetaxo[,c("espar","species_name","french_name")],by="espar",all.x = T)
+  NFI_tree<-NFI_tree[veget!="",]
   # NFI_plot_info<<-NFI_plot_info[order(idp),]
-  # NFI_ecologie<<-NFI_ecologie[order(idp),]
-  # NFI_arbres<<-NFI_arbres[order(idp),]
+  # NFI_ecology<<-NFI_ecology[order(idp),]
+  # NFI_tree<<-NFI_tree[order(idp),]
   # NFI_dendro<<-NFI_dendro[order(idp),]
   # NFI_cover<<-NFI_cover[order(idp),]
   # NFI_flora<<-NFI_flora[order(idp),]
 
   if(write_csv){
     write.table(NFI_plot_info,file=file.path(path_data,"plot_info.csv"),sep=";",row.names = F)
-    write.table(NFI_ecologie,file=file.path(path_data,"ecology.csv"),sep=";",row.names = F)
-    write.table(NFI_arbres,file=file.path(path_data,"tree.csv"),sep=";",row.names = F)
+    write.table(NFI_ecology,file=file.path(path_data,"ecology.csv"),sep=";",row.names = F)
+    write.table(NFI_tree,file=file.path(path_data,"tree.csv"),sep=";",row.names = F)
     write.table(NFI_dendro,file=file.path(path_data,"dendro.csv"),sep=";",row.names = F)
     write.table(NFI_cover,file=file.path(path_data,"canopy_cover.csv"),sep=";",row.names = F)
     write.table(NFI_flora,file=file.path(path_data,"flora.csv"),sep=";",row.names = F)
@@ -177,8 +177,8 @@ get_NFI<-function(dir=getwd(),
 
   if(export_to_env){
     NFI_plot_info<<-NFI_plot_info[order(idp),]
-    NFI_ecologie<<-NFI_ecologie[order(idp),]
-    NFI_arbres<<-NFI_arbres[order(idp),]
+    NFI_ecology<<-NFI_ecology[order(idp),]
+    NFI_tree<<-NFI_tree[order(idp),]
     NFI_dendro<<-NFI_dendro[order(idp),]
     NFI_cover<<-NFI_cover[order(idp),]
     NFI_flora<<-NFI_flora[order(idp),]
@@ -190,9 +190,39 @@ get_NFI<-function(dir=getwd(),
 
 
 
-  if(!export_to_env)return(list(NFI_plot_info,NFI_ecologie,NFI_arbres,NFI_dendro,NFI_cover,NFI_flora)) else return(NULL)
+  if(!export_to_env)return(list(NFI_plot_info,NFI_ecology,NFI_tree,NFI_dendro,NFI_cover,NFI_flora)) else return(NULL)
 
 }
 
 
+#' @title Compute dendrological variable for one species at the plot scale
+#'
+#'
+#' @return a data.table of 2 dendrological variable (basal area of the species, and its number of stem per hectares) and the plot id idp
+#' @author J Borderieux (jeremy.borderieux@@agroparistech.fr)
+#' @param NFI_tree The data.table storing the tree data, usually **NFI_tree** retured by {\link[=get_NFI]{get_NFI}}
+#' @param species_name A Character indicating the NFI species code, look for "espar" in the metadata.csv for more informations
+#' @references https://inventaire-forestier.ign.fr/
+#' @examples \dontrun{
+#'
+#' NFI_douglas<-compute_dendro_sp(NFI_tree,"64")
+#' head(NFI_douglas)
+#' }
+#' @export
+compute_dendro_sp<- function (NFI_tree, species_name) {
 
+  tmp<-NFI_tree[veget%in%c("0","Z"),]
+  tmp$ba<-ifelse(tmp$espar==species_name,tmp$gFinal,0)
+  tmp$wtot<-ifelse(tmp$espar==species_name,tmp$w,0)
+
+   tmp <- tmp[,list(basal_area=sum(ba,na.rm=T),
+                     Ntot=sum(wtot,na.rm=T)),
+                    by = idp]
+  # tmp<-NFI_tree
+  # tmp[,basal_area:=]
+
+  colnames(tmp) <- paste0(colnames(tmp),"_",species_name)
+  colnames(tmp)[1]<-"idp"
+  return(tmp)
+
+}
